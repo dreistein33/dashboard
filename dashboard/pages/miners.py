0853052fad94efc08e359ed2f221be682203fb6c
Kuba <106917950+dreistein33@users.noreply.github.com>
2022-08-04 14:50:35 -0400

    Update miners.py

--- dashboard/pages/miners.py
+++ dashboard/pages/miners.py
@@ -10,30 +10,35 @@ pholder = st.empty()
 while True:
     source = ethermine.miner_current_stats(ad)
     list_of_workers = [x for x in ethermine.miner_workers(ad)]
+    
     with pholder.container():
         left_column, middle_column = st.columns([2, 1.5])
-        left_column.header('MURZYNY:')
+        left_column.header('WORKERS:')
         left_column.write('---')
+        
         for items in list_of_workers:
             left_column.metric(items['worker'], items['reportedHashrate']/10**6)
             if items['reportedHashrate'] < 100:
                 left_column.warning('COS SIE ZEPSULO!')
 
         reported_rate = float(source['reportedHashrate']/10**6)
+        
         usd_per_min = source['usdPerMin']
         coin_per_min = source['coinsPerMin']
         btc_per_min = source['btcPerMin']
+        
         uhour = (usd_per_min * 60) / reported_rate
         chour = coin_per_min / reported_rate * 60
         btchour = btc_per_min / reported_rate * 60
+        
         middle_column.header('1MH MOC')
         middle_column.write('---')
         middle_column.write(f'1MH = {uhour:.16f} USD PER HOUR')
         middle_column.write(f'1MH = {chour:.16f} ETH PER HOUR')
         middle_column.write(f'1MH = {btchour:.16f} BTC PER HOUR')
         middle_column.write('---')
-        middle_column.header('MEGAHUJASZE ZA GODZINKE;)')
-        middle_column.subheader(f'{reported_rate} MEGAHUJASZY')
+        middle_column.header('MEGAJUHASZE ZA GODZINKE;)')
+        middle_column.subheader(f'{reported_rate} MEGAJUHASZY')
         middle_column.write(f'{usd_per_min * 60} USD')
         middle_column.write(f'{coin_per_min * 60} ETH')
         
